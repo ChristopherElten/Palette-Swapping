c7e65f42e6d4065b3d20582014e7bfcfcaaf662e
ChristopherElten <Chris.Elten@gmail.com>
2015-06-19 23:20:22 -0400

    Initial Commit
    Palette Swapping for efficient use of limited available graphics in
    future unity projects.

---
+++ Assets/Scripts/ColorPalette.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEditor;
+
+[Serializable]
+public class ColorPalette : ScriptableObject {
+
+	[MenuItem("Assets/Create/Color Palette")]
+	public static void CreateColorPalette(){
+
+		//Checking for correct file type
+		if(Selection.activeObject is Texture2D){
+
+			var selectedTexture = Selection.activeObject as Texture2D;
+			var selectionPath = AssetDatabase.GetAssetPath(selectedTexture);
+			//Creating asset path with appended name
+			selectionPath = selectionPath.Replace(".png", "-color-palette.asset");
+			var newPalette = CustomAssetUtil.CreateAsset<ColorPalette>(selectionPath);
+
+			Debug.Log ("Creating a Palette : " + selectionPath);
+		} else {
+			Debug.Log ("Can't create a Palette");
+		}
+
+	}
+	
+}
---
+++ Assets/Scripts/CustomAssetUtil.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CustomAssetUtil {
+	
+	public static T CreateAsset<T>(string path) where T : ScriptableObject{
+
+		T asset = null;
+
+		asset = ScriptableObject.CreateInstance<T> ();
+
+		var newPath = AssetDatabase.GenerateUniqueAssetPath(path);
+
+		AssetDatabase.CreateAsset(asset, newPath);
+
+		AssetDatabase.SaveAssets();
+
+		return asset;
+
+	}
+}
